d013f09970e710161cbfc1ba39cea5c2d41d2c9b
antonioortegajr <antonioortegajr@gmail.com>
2016-04-17 15:31:30 -0700

    updated with proper functions
    updated with proper functions

--- WP_JumpOff.php
+++ WP_JumpOff.php
@@ -11,16 +11,42 @@ Author URI: http://antonioortegajr.com
 License: GPL2
 */
 
-// use the WP function to enqueue the script. https://developer.wordpress.org/reference/functions/wp_enqueue_script/
-//remove this enqueue and the folder/file if you do not need this
-wp_enqueue_script('jumpoff_script', plugins_url('js/script.js', dirname(__FILE__)));
+// use the WP function to enqueue the script. //https://developer.wordpress.org/reference/functions/wp_enqueue_script/
 
-// use the WP function to enqueue the style. https://developer.wordpress.org/reference/functions/wp_enqueue_style/
-//remove this enqueue and the folder/file if you do not need this
-wp_enqueue_style('jumpoff_style', plugins_url('style/style.csss', dirname(__FILE__)));
+//remove these enqueues and the folder/file if you do not need these
+
+function wp_jump_off_script() {
+	wp_enqueue_script( 'wp_jump_off_js', plugins_url('WP_JumpOff-master/js/script.js', dirname(__FILE__)) );
+}
+
+function wp_jump_off_enqueue_style() {
+	wp_enqueue_style( 'wp_jump_off_css', plugins_url('WP_JumpOff-master/style/style.css', dirname(__FILE__)) );
+}
+
+
+add_action( 'wp_enqueue_scripts', 'wp_jump_off_enqueue_script' );
+add_action( 'wp_enqueue_scripts', 'wp_jump_off_enqueue_style' );
+
 
 //include the file for a WP API endpoint. requires WP 4.4 or higher. http://wp-api.org/guides/extending.html
-//remove this include and the folder/file if you do not need an API endpoint
-include('api/routes.php');
+
+//remove this if you do not need an API endpoint
+add_action( 'rest_api_init', function () {
+  //set up routes
+    register_rest_route( 'wp_jump_off/v1/', 'route', array(
+        'methods' => 'GET',
+        'callback' => 'wp_jump_off_func',
+    ) );
+
+} );
+
+
+//WP API Return
+function wp_jump_off_func( WP_REST_Request $request ){
+return '{"example":"test"}';
+}
+
+
+
 
 ?>
